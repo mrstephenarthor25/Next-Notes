bcaa336de9af64b0201a2b507523f75b5028c172
stephen <mrstephenarthor25@gmail.com>
2025-09-14 12:57:15 -0700

    added sitemap generator

---
+++ app/sitemap.ts
@@ -0,0 +1,27 @@
+import { MetadataRoute } from "next";
+import { getAllContents } from "./_db/content";
+
+export default async function sitemap():Promise<MetadataRoute.Sitemap>{
+
+    const domain = "https:\\vercel.next-notes.app/";
+
+    //get all the pages 
+    let contents = await getAllContents();
+    let url =  contents.map( (content)=>{
+        return {
+            url:`domain${content.slug}`,
+            lastModified:new Date(),
+            priority:1
+        }
+    });
+
+   //add home page at the begining
+    [{
+        url:domain,
+        lastModified:new Date(),
+        priority:1
+    }].push(...url)
+
+    return url;
+
+}
